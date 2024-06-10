df31e57b9adba7a25045cef35e485c5aa4fbc8e0
rahul_kgarrcz <ranikumari.edugaon@gamil.com>
2024-06-10 12:44:26 +0530

    create and add ui

--- lib/ui_screen.dart
+++ lib/ui_screen.dart
@@ -8,18 +8,216 @@ class UiScreen extends StatefulWidget {
 }
 
 class _UiScreenState extends State<UiScreen> {
+  bool isPrivate = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Card(
-            child: Row(
+      appBar: AppBar(
+        title: const Text("Notifications"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(12),
+        child: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(right: 185),
+              child: Text(
+                "Push notifications",
+                style: TextStyle(fontSize: 23),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                  Icon(Icons.notifications),
-                   Text("Notifications"),
-                   Icon(Icons.arrow_forward_ios_rounded),
+                const Padding(
+                  padding: EdgeInsets.only(left: 5, top: 20),
+                  child: Text(
+                    "Pause all",
+                    style: TextStyle(fontSize: 23),
+                  ),
+                ),
+                Switch(
+                  activeColor: Colors.black,
+                  value: isPrivate,
+                  onChanged: (value) {
+                    setState(() {
+                      isPrivate = value;
+                    });
+                  },
+                )
+               ,
+                // Icon(Icons.notifications)
               ],
             ),
+            Padding(
+              padding: EdgeInsets.only(right: 170, top: 20),
+              child: Text(" Temporarily pause notifications"),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 240, top: 20),
+              child: Text(
+                "Quiet mode ",
+                style: TextStyle(fontSize: 23),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 10, top: 20),
+              child: Text(
+                "Automatically  mute notifications at night or whenever you need to focus ",
+                style: TextStyle(fontSize: 11),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 5, top: 20),
+                  child: Text(
+                    "Posts , stories and comments",
+                    style: TextStyle(fontSize: 19),
+                  ),
+                ),
+                Icon(Icons.arrow_forward_ios_rounded)
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 5, top: 20),
+                  child: Text(
+                    "Following and followers",
+                    style: TextStyle(fontSize: 19),
+                  ),
+                ),
+                Icon(Icons.arrow_forward_ios_rounded)
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 5, top: 20),
+                  child: Text(
+                    "Messages",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                Icon(Icons.arrow_forward_ios_rounded)
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 5, top: 20),
+                  child: Text(
+                    "Calls",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                Icon(Icons.arrow_forward_ios_rounded)
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 5, top: 20),
+                  child: Text(
+                    "Live and reels",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                Icon(Icons.arrow_forward_ios_rounded)
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 5, top: 20),
+                  child: Text(
+                    "Fundraisers",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                Icon(Icons.arrow_forward_ios_rounded)
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 5, top: 20),
+                  child: Text(
+                    "From Instagram",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                Icon(Icons.arrow_forward_ios_rounded)
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 5, top: 20),
+                  child: Text(
+                    "Birthday",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                Icon(
+                  Icons.arrow_forward_ios_rounded,
+                )
+              ],
+            ),
+            Divider(
+              color: Colors.black,
+              height: 20,
+              thickness: BorderSide.strokeAlignCenter,
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 135),
+              child: Text(
+                "Other notifications types",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 5, top: 20),
+                  child: Text(
+                    "Email notifications",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                Icon(
+                  Icons.arrow_forward_ios_rounded,
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 5, top: 20),
+                  child: Text(
+                    "Shopping",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                Icon(
+                  Icons.arrow_forward_ios_rounded,
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
