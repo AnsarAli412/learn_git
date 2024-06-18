96e1d4477ce92523ec1a6a628f7c44eb90fabcd0
Jyoti kumari <kumarijyoti58301gmail.com>
2024-06-18 17:05:19 +0530

    created project

--- lib/age_filter_screen.dart
+++ lib/age_filter_screen.dart
@@ -1,26 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
-import 'package:flutter/material.dart';
-
-import 'age_filter_screen.dart';
-import 'ordres_ui_screen.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
-}
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
 
